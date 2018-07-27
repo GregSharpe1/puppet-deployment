0c83cc534634c7b92c0ffe23d3a2748713f91f67
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-27 15:48:58 +0100

    Attempting some wordpress stuff

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,1 +1,13 @@
-class { "nginx" : }
+package { "nginx": }
+service { "nginx":
+  ensure     => "running",
+  hasrestart => true,
+  hasstatus  => true,
+  require    => Package["nginx"]
+}
+file { "/etc/nginx/sites-available/default":
+  content => template("../templates/default-site.erb"),
+  require => Package["nginx"],
+  notify  => Service["nginx"]
+}
+
