b82056bf3358e830b343d4ab627bced5e16bde39
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-27 12:28:06 +0100

    Installing mysql

--- manifests/mysql.php
+++ manifests/mysql.php
@@ -1,6 +1,8 @@
 include '::mysql::server'.
 
 class { '::mysql::server':
-  root_password           => 'superduperstrong',
+  package_name            => 'mysql-server',
+  package_ensure          => '5.7.1+mysql~trusty',
+  root_password           => 'strongpassword',
   remove_default_accounts => true,
 }
