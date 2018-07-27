a7bcb997d4af6dbc85ad2bd3ec72b6034a5ed81b
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-27 12:41:44 +0100

    Fucking install mysql you mug

--- manifests/mysql.php
+++ manifests/mysql.php
@@ -1,8 +1,4 @@
-include '::mysql::server'.
-
 class { '::mysql::server':
-  package_name            => 'mysql-server',
-  package_ensure          => '5.7.1+mysql~trusty',
   root_password           => 'strongpassword',
   remove_default_accounts => true,
 }
