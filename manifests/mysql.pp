7837b3841fe6d7e41c9f3305ff782e3be53e11b9
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-29 12:33:19 +0100

    remove

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,9 +1,3 @@
 class { '::mysql::server': }
 
 # Install mysql server then create the wordpress database
-mysql::db { 'wordpress':
-  user => 'wordpress',
-  password => 'wordpress_password',
-  host => 'localhost',
-  grant => ['ALL']
-}
