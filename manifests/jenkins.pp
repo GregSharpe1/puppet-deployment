1b6dcac149b5395d8d509818f751ea58d072cf3f
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-30 16:22:15 +0100

    reverting changes

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,4 +1,14 @@
 node 'jenkins' {
-  class { "jenkins" :}
-  class { "java" : }
+  include jenkins
+
+  class { 'java' :
+    package => 'java-1.8.0-openjdk-devel',
+  }
+
+  # Java install
+  java::oracle { 'jdk8' :
+    ensure  => 'present',
+    version => '8',
+    java_se => 'jdk',
+  }
 }
