49fcf2a396cdefcbc4bac25c5a9807c38cead9e5
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-30 16:14:30 +0100

    Including java

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,14 +1,4 @@
 node 'jenkins' {
   include jenkins
-
-  class { 'java' :
-    package => 'java-1.8.0-openjdk-devel',
-  }
-
-  # Java install
-  java::oracle { 'jdk8' :
-    ensure  => 'present',
-    version => '8',
-    java_se => 'jdk',
-  }
+  include java
 }
