8d00d35848d7f023934bb0c5cb9bd84a41c635d0
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-30 09:58:14 +0100

    Imstalled MySQL and created DB

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,6 +1,9 @@
-class { '::mysql::server': 
-  root_password           => 'password',
-  remove_default_accounts => true,
-}
+class { '::mysql::server': }
 
+mysql::db {'wordpress':
+  user => 'wordpress',
+  password => 'password',
+  host => 'localhost',
+  ensure => 'present',
+}
 # Install mysql server then create the wordpress database
