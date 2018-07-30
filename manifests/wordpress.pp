c74cd8c2320c247d2a7bbd882aa99935a5d57aee
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-30 14:35:40 +0100

    Attempting to fix the wordpress install

--- manifests/wordpress.pp
+++ manifests/wordpress.pp
@@ -1,7 +1,7 @@
 class { 'wordpress':
-  wp_owner    => 'root',
-  wp_group    => '0',
-  db_user        => "root",
-  db_password    => "password",
-  install_dir => '/var/www/html',
+  # wp_owner    => 'root',
+  # wp_group    => '0',
+  # db_user        => "root",
+  # db_password    => "password",
+  # install_dir => '/var/www/html',
 }
