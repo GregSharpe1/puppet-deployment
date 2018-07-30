65df8ea357a58225718f3d2c8cbe694477e07dee
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-30 16:42:27 +0100

    Setting up jenkins as per video

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,11 +1,6 @@
 node 'jenkins' {
-  include jenkins
-  include java
-
-  # Java install
-  java::oracle { 'jdk8' :
-    ensure  => 'present',
-    version => '8',
-    java_se => 'jdk',
+  class {'jenkins' :
+    install_java => true,
+    cli => true
   }
 }
