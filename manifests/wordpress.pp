e1e13068c268a6d8610328d92a13a50486a34766
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-30 14:49:48 +0100

    Editing the index.html path

--- manifests/wordpress.pp
+++ manifests/wordpress.pp
@@ -1,12 +1,8 @@
 class { 'wordpress':
-  # wp_owner    => 'root',
-  # wp_group    => '0',
-  # db_user        => "root",
-  # db_password    => "password",
   install_dir => '/var/www/html',
 }
 
 # Once apache is installed we need to remove the default file
-file { '/var/www/index.html':
+file { '/var/www/html/index.html':
     ensure  => 'absent',
 }
