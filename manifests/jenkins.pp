f68faf9c491230e0509bafa3fd5f2e2cd872ca0d
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-31 11:06:32 +0100

    sy

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -20,32 +20,32 @@ node 'jenkins' {
   # https://jenkins.io/doc/book/installing/
 
   # First step is to add the gpg key to the list of trusted keys
-  # include apt
 
-  # apt::key{ 'jenkins-ci':
-  #   server => 'https://pkg.jenkins.io/debian/jenkins.io.key'
+
+  apt::key{ 'jenkins-ci':
+    server => 'https://pkg.jenkins.io/debian/jenkins.io.key'
+  }
+
+  # exec { 'added jenkins apt key':
+  #   command => "wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -"    
   # }
 
-  # # exec { 'added jenkins apt key':
-  # #   command => "wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -"    
-  # # }
-
-  # apt::source { 'jenkins_ci':
-  #   comment => "Adding the Jenkins-ci source",
-  #   location => "http://pkg.jenkins.io/debain-stable",
-  #   release => "stable",
-  #   include => {
-  #     'deb' => true,
-  #   },
-  # }
+  apt::source { 'jenkins_ci':
+    comment => "Adding the Jenkins-ci source",
+    location => "http://pkg.jenkins.io/debain-stable",
+    release => "stable",
+    include => {
+      'deb' => true,
+    },
+  }
 
   # # exec { 'deb to apt jenkins.list': 
   # #   command => sudo sh -c ' echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
   # # }
 
-  # package { 'jenkins':
-  #   name => 'jenkins',
-  #   ensure => installed,
-  #   require => Exec['apt-get update'],
-  # }
+  package { 'jenkins':
+    name => 'jenkins',
+    ensure => installed,
+    require => Exec['apt-get update'],
+  }
 }
