b572cc56182cb662b49133c88cd94d774a986666
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-31 10:16:42 +0100

    Attempting to use the apt module:

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -5,13 +5,28 @@ node 'jenkins' {
   # https://jenkins.io/doc/book/installing/
 
   # First step is to add the gpg key to the list of trusted keys
-  exec { 'added jenkins apt key':
-    command => "wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -"    
+  include apt
+
+  apt::key{ 'jenkins-ci':
+    server => 'https://pkg.jenkins.io/debian/jenkins.io.key'
   }
 
-  exec { 'deb to apt jenkins.list': 
-    command => sudo sh -c ' echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+  # exec { 'added jenkins apt key':
+  #   command => "wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -"    
+  # }
+
+  apt:source { 'jenkins_ci':
+    comment => "Adding the Jenkins-ci source",
+    location => "http://pkg.jenkins.io/debain-stable"
+    release => "stable"
+    include => {
+      'deb' => true,
+    },
   }
+
+  # exec { 'deb to apt jenkins.list': 
+  #   command => sudo sh -c ' echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+  # }
 
   package { 'jenkins':
     name => 'jenkins'
