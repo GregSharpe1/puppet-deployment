14e5d7922129c92886e79b8c997eee53930a9476
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-31 10:40:39 +0100

    Installing java8

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,36 +1,41 @@
 node 'jenkins' {
   # We have decided to attempt to create the manifest ourselves
+  java::oracle { 'jdk8' :
+    ensure  => 'present',
+    version => '8',
+    java_se => 'jdk',
+  }
 
   # According to the installion docs from Jenkins.io 
   # https://jenkins.io/doc/book/installing/
 
   # First step is to add the gpg key to the list of trusted keys
-  include apt
+  # include apt
 
-  apt::key{ 'jenkins-ci':
-    server => 'https://pkg.jenkins.io/debian/jenkins.io.key'
-  }
-
-  # exec { 'added jenkins apt key':
-  #   command => "wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -"    
+  # apt::key{ 'jenkins-ci':
+  #   server => 'https://pkg.jenkins.io/debian/jenkins.io.key'
   # }
 
-  apt::source { 'jenkins_ci':
-    comment => "Adding the Jenkins-ci source",
-    location => "http://pkg.jenkins.io/debain-stable",
-    release => "stable",
-    include => {
-      'deb' => true,
-    },
-  }
+  # # exec { 'added jenkins apt key':
+  # #   command => "wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -"    
+  # # }
 
-  # exec { 'deb to apt jenkins.list': 
-  #   command => sudo sh -c ' echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+  # apt::source { 'jenkins_ci':
+  #   comment => "Adding the Jenkins-ci source",
+  #   location => "http://pkg.jenkins.io/debain-stable",
+  #   release => "stable",
+  #   include => {
+  #     'deb' => true,
+  #   },
   # }
 
-  package { 'jenkins':
-    name => 'jenkins',
-    ensure => installed,
-    require => Exec['apt-get update'],
-  }
+  # # exec { 'deb to apt jenkins.list': 
+  # #   command => sudo sh -c ' echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+  # # }
+
+  # package { 'jenkins':
+  #   name => 'jenkins',
+  #   ensure => installed,
+  #   require => Exec['apt-get update'],
+  # }
 }
