0f1a4027093845724245b83b7a5dffa216bc33c0
Greg Sharpe <gregsharpe3@gmail.com>
2018-07-31 09:49:49 +0100

    First test of the Jenkins manifest/module

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,3 +1,21 @@
 node 'jenkins' {
-  include jenkins
+  # We have decided to attempt to create the manifest ourselves
+
+  # According to the installion docs from Jenkins.io 
+  # https://jenkins.io/doc/book/installing/
+
+  # First step is to add the gpg key to the list of trusted keys
+  exec { 'added jenkins apt key':
+    command => "wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -"    
+  }
+
+  exec { 'deb to apt jenkins.list': 
+    command => sudo sh -c ' echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+  }
+
+  package { 'jenkins':
+    name => 'jenkins'
+    ensure => installed,
+    require => Exec['apt-get update']
+  }
 }
