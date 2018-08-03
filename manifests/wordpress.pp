608b033d0db5985edde85576a36a225ea699b230
Greg Sharpe <gregsharpe3@gmail.com>
2018-08-03 16:13:09 +0100

    Attmepting something

--- manifests/wordpress.pp
+++ manifests/wordpress.pp
@@ -1,33 +1,10 @@
 node 'wordpress' {
 
-  # MySQL related shit
-  class { '::mysql::server': 
-      remove_default_accounts => true,
-  }
+class { 'jenkins': ; }
 
-  include mysql::server
-
-  # Wordpress related 
-  class { 'wordpress':
-    install_dir => '/var/www/html',
-  }
-
-  # Apache related goodness
-  class { 'apache':  
-    mpm_module => 'prefork'
-  }
-  # Once apache is installed we need to remove the default file
-  file { '/var/www/html/index.html':
-      ensure  => 'absent',
-  }
-
-  # PHP related sexyiness
-  include apache::mod::php
-
-  class { "php": }
-
-  # Install a custom php module
-  package { 'php5-mysqlnd-ms':
-    ensure => installed,
-  }
+install-jenkins-plugin {
+    "git-plugin" :
+        name    => "git,
+        version => "1.1.11";
+}
 }
