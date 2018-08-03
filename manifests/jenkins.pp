1f63cdac570fc0b223a4a52270cb3fd0456009d1
jakemorganDOG <jakeelliotmorgan@gmail.com>
2018-08-03 11:55:05 +0100

    changed ordering

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -49,7 +49,7 @@ node 'jenkins' {
     name => 'jenkins',
     ensure  => running,
     enable  => true,
-    require => Exec['apt-update']
+    require => Exec['apt-update'],
     before =>  Exec['add jenkins java variable']
   }
 
