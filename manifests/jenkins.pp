e2e952a37c67171f92444cc79cc24adb1191bcba
Greg Sharpe <gregsharpe3@gmail.com>
2018-08-03 15:07:58 +0100

    Let's just try something

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -5,7 +5,6 @@ node 'jenkins' {
   include apt
 
   apt::ppa{ 'ppa:jonathonf/openjdk':
-    before => Exec['apt-update'],
   }
 
   # According to the installion docs from Jenkins.io
@@ -15,7 +14,6 @@ node 'jenkins' {
   apt::key {
     'D50582E6':
       source => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
-    before => Exec['apt-update'],
   }
 
   apt::source { 'jenkins':
@@ -26,37 +24,32 @@ node 'jenkins' {
     include => {
       'deb' => true,
     },
-    before => Exec['apt-update'],
   }
 
   exec { "apt-update":
-    command => "/usr/bin/apt-get update"
+    command => "/usr/bin/apt-get update",
   }
 
   package { 'java8':
     name => "openjdk-8-jdk",
     ensure => installed,
-    require => Exec['apt-update'],
   }
 
   package { 'jenkins':
     name => 'jenkins',
     ensure => "2.73.3",
-    require => Exec['apt-update'],
   }
 
   service { 'start jenkins':
     name => 'jenkins',
     ensure  => running,
     enable  => true,
-    require => Package['jenkins'],
   }
 
   # Before starting the jenkins service we must edit the /etc/default/jenkins file
   # to allow the -Djenkins.install.runSetupWizard=false flag under JAVA_OPTION variable.
   exec { 'add jenkins java variable':
     command => '/bin/sed -i "s#JAVA_ARGS=\"-Djava.awt.headless=true\"#JAVA_ARGS=\"-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false\"#g" /etc/default/jenkins',
-    require => Package['jenkins'],
     notify => Service['start jenkins']
   }
 
@@ -64,6 +57,10 @@ node 'jenkins' {
     command => '/bin/sed -i "s#<useSecurity>true#<useSecurity>false#g" /var/lib/jenkins/config.xml',
     require => Package['jenkins'],
     notify => Service['start jenkins']
+  }
+
+  exec { 'restart jenkins': 
+    command => "/usr/sbin/service jenkins restart"
   }
 
 }
